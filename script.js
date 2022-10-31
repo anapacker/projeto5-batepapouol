ea1424a941741aa3952bdcb8123a5cb53dcfabee
Ana Packer <anaamaria.packer@gmail.com>
2022-10-31 16:02:41 -0300

    implementado filtro de  mensagens novas

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 let boxMsgHTML = "";
+const objName = {
+    name: ""
+
+}
+let ultimaMensagem;
 entrarNaSala()
 buscarMsgServidor();
-setInterval(buscarMsgServidor, 3000);
+setInterval(continuarPegandoMsgNovas, 3000);
 
 
 function buscarMsgServidor() {
@@ -9,8 +14,83 @@ function buscarMsgServidor() {
     promessa.then(construirMsgNaTela);
 }
 
+
+function continuarPegandoMsgNovas() {
+    const promessa = axios.get('https://mock-api.driven.com.br/api/v6/uol/messages');
+    promessa.then(filtrarMsgsNovas);
+    // promessa.catch()
+}
+
+function filtrarMsgsNovas(resposta) {
+    const todasAsMensagens = resposta.data;
+    let mensagensNovas = [];
+    let indexMensagem = null;
+    for (let i = 0; i < todasAsMensagens.length; i++) {
+
+        if (todasAsMensagens[i].from === ultimaMensagem.from && todasAsMensagens[i].time === ultimaMensagem.time) {
+            indexMensagem = i + 1;
+        }
+    }
+
+    if (indexMensagem < todasAsMensagens.length - 1) {
+        for (let j = indexMensagem; j < todasAsMensagens.length; j++) {
+            mensagensNovas.push(todasAsMensagens[j])
+        }
+    }
+
+    if (mensagensNovas.length > 0) {
+        colocarNovasMensagensNaTela(mensagensNovas)
+    }
+}
+
+function colocarNovasMensagensNaTela(mensagensNovas) {
+
+    // if (mensagensNovas.length > 0) {
+
+    const listaMensagens = mensagensNovas;
+    ultimaMensagem = listaMensagens[listaMensagens.length - 1]
+
+    boxMsgHTML = "";
+    for (let i = 0; i < listaMensagens.length; i++) {
+
+        let objetoMsg = listaMensagens[i];
+        let tipoMsg = objetoMsg.type;
+        let msgHTML = "";
+
+        if (tipoMsg === "status") {
+            msgHTML =
+                `
+                        <li class="mensagem status">
+                            <span class="horario-msg">(${objetoMsg.time})</span>
+                            <span class="remetente-msg"><strong>${objetoMsg.from}</strong> </span>
+                            <span class="status-msg">${objetoMsg.text}</span>
+                        </li>
+                  
+                `
+        } else if (tipoMsg === "message") {
+            msgHTML =
+                `<li class="mensagem">
+                    <span class="horario-msg">(${objetoMsg.time})</span>
+                    <span class="enviado-por">${objetoMsg.from}</strong></span>
+                    <span class="remetente-msg"><strong>${objetoMsg.to}:</strong></span>
+                    <span class="conteudo-msg">${objetoMsg.text}</span>
+                </li>`
+
+        }
+
+        boxMsgHTML += msgHTML;
+
+    }
+    document.querySelector(".box-mesages").innerHTML += boxMsgHTML;
+    // }
+
+}
+
 function construirMsgNaTela(resposta) {
     const listaMensagens = resposta.data;
+    ultimaMensagem = listaMensagens[listaMensagens.length - 1]
+
+
     boxMsgHTML = "";
     for (let i = 0; i < listaMensagens.length; i++) {
 
@@ -49,12 +129,9 @@ function construirMsgNaTela(resposta) {
 
 function entrarNaSala() {
     const askName = prompt("Digite seu nome");
-    const objName = {
-        name: askName
-
-    }
+    objName.name = askName;
     const promessa = axios.post("https://mock-api.driven.com.br/api/v6/uol/participants", objName);
-    promessa.then();
+    promessa.then(definirIntervaloManterConexao);
     promessa.catch(tratarErroLogin);
 
 
@@ -64,4 +141,14 @@ function entrarNaSala() {
 function tratarErroLogin() {
     alert("Esse nome já existe");
     entrarNaSala();
+}
+
+function definirIntervaloManterConexao() {
+    setInterval(enviarNome, 4000);
+}
+
+function enviarNome() {
+    const promessa = axios.post("https://mock-api.driven.com.br/api/v6/uol/status", objName);
+    promessa.then();
+    promessa.catch();
 }
